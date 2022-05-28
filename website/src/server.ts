5aa0db3fe6ad5cae8efb0072300eb2dda0bd1c08
FreezB11 <yashrajrao4@gmail.com>
2022-05-28 18:59:36 +0530

    init

--- website/src/server.ts
+++ website/src/server.ts
@@ -24,10 +24,7 @@ const userSchema = new Schema<User>({
     name: { type: String, required: true },
     email: { type: String, required: true },
     avatar: String,
-    address:{
-        street: {type:String,required:true},
-        city: {type:String, required: true}
-    }
+    address:String
 });
 
 const User = model<User>('User', userSchema);
