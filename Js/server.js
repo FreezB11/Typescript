b3a4280c6cb5f0c4666237c198958a635551af32
FreezB11 <yashrajrao4@gmail.com>
2022-05-28 20:45:57 +0530

    init

--- Js/server.js
+++ Js/server.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const express = require('express');
+const server = express()
+
+
+server.set('view engine', 'ejs');
+
+server.get('/',(req,res)=>{
+    res.render("views/index")
+})
+
+server.listen(8000, ()=>{
+    console.log("server started")
+})
