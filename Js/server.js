222e3ac199ec6367b8832c716c117d9ca376e23a
FreezB11 <yashrajrao4@gmail.com>
2022-05-29 20:52:16 +0530

    init

--- Js/server.js
+++ Js/server.js
@@ -5,6 +5,9 @@ const server = express()
 server.set('views', path.join(__dirname, './views'));
 server.set('view engine', 'ejs');
 
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
 server.get('/',(req,res)=>{
     res.render("views/index")
 })
