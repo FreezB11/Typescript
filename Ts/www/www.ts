91faeb531ba9815e9ad8a530168f7d765b0ff4d3
FreezB11 <yashrajrao4@gmail.com>
2022-05-30 19:20:59 +0530

    init

--- Ts/www/www.ts
+++ Ts/www/www.ts
@@ -3,10 +3,23 @@ import http from 'http';
 import logging from'../src/config/logging';
 import config from '../src/config/config';
 
-
 const NAMESPACE = 'Server';
 
 const httpServer = http.createServer(router);
+
+router.use((req, res, next) => {
+    /** Log the req */
+    logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        /** Log the res */
+        logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`);
+    })
+    
+    next();
+});
+
+
 
 httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
 
