3fd7f5b3a5ef2006308717bec685c3d7b65e6a39
FreezB11 <yashrajrao4@gmail.com>
2022-05-31 19:16:30 +0530

    init

--- Ts/src/server.ts
+++ Ts/src/server.ts
@@ -12,17 +12,12 @@ interface User {
     name: string;
     email: string;
     avatar?: string;
-    address:{
-        street: string,
-        city:string,
-    }
 }
 
 const userSchema = new Schema<User>({
     name: { type: String, required: true },
     email: { type: String, required: true },
     avatar: String,
-    address:String
 });
 
 const User = model<User>('User', userSchema);
