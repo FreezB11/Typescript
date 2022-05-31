be791d486202675bb5e472edf8e7c85eadd1f104
FreezB11 <yashrajrao4@gmail.com>
2022-05-31 19:33:22 +0530

    init

--- Ts/src/views/404.ts
+++ Ts/src/views/404.ts
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+import { NextFunction, Request, Response } from 'express';
+
+
+const index = (req: Request, res: Response, next: NextFunction) => {
+    return res.send(`
+    <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,3 +16,7 @@
     
 </body>
 </html>
+    `)
+};
+
+export default {index} 
