86e32449a79aaab5c02f6130c6bfbadeb2804e60
FreezB11 <yashrajrao4@gmail.com>
2022-06-01 19:56:12 +0530

    init

--- Ts/www/www.ts
+++ Ts/www/www.ts
@@ -7,4 +7,8 @@ const NAMESPACE = 'Server';
 
 const httpServer = http.createServer(router);
 
+
+
 httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
+
+export = httpServer;
