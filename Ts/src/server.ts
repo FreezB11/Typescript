c53eda8fc724f2353f7a1afe14f5a2f9e77139f7
FreezB11 <yashrajrao4@gmail.com>
2022-06-02 21:56:54 +0530

    init

--- Ts/src/server.ts
+++ Ts/src/server.ts
@@ -7,7 +7,7 @@ import connect = require("./database/database")
 import { Schema, model} from 'mongoose';
 import httpServer from '../www/www'
 
-import { Server } from "socket.io";
+import ioserver, { Server } from "socket.io";
 
 
 
