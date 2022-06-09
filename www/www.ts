26c7b8d5ee0df316e628cdb08fe2d8cfb8ea250f
FreezB11 <yashrajrao4@gmail.com>
2022-06-09 20:55:51 +0530

    init

--- www/www.ts
+++ www/www.ts
@@ -2,8 +2,10 @@ import router from "../src/server";
 import httpServer from "../src/server"
 import logging from'../src/config/logging';
 import config from '../src/config/config';
-
+import { Server } from "socket.io";
 const NAMESPACE = 'Server';
+
+
 
 httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
 
