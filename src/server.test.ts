e484491a13c590037e7a10f8332796765b929a80
FreezB11 <yashrajrao4@gmail.com>
2022-06-11 19:56:03 +0530

    init

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,24 +1,24 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 class App {
- public app: Application;
- constructor() {
-  this.app = express();
-  this.config();
- }
- private config(): void {
- this.app.use((req: Request, res: Response, next: NextFunction) => {
- res.header('Access-Control-Allow-Origin', '*');
- res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
-res.header('Access-Control-Allow-Headers', '*');
-next();
-});
-this.app.use(bodyParser.json());
-this.app.use(bodyParser.urlencoded({ extended: false }));
+    public app: Application;
+    constructor() {
+        this.app = express();
+        this.config();
+    }
+    private config(): void {
+        this.app.use((req: Request, res: Response, next: NextFunction) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
+        res.header('Access-Control-Allow-Headers', '*');
+        next();
+    });
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
 }
-}
+
+const server = new App()
 
 
-
-const port = 3000;
 
