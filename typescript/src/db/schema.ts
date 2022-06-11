3f3c95848937b4163aa69c247222907934cbad28
FreezB11 <yashrajrao4@gmail.com>
2022-06-11 21:37:55 +0530

    init

---
+++ typescript/src/db/schema.ts
@@ -0,0 +1,17 @@
+import { Schema, model} from 'mongoose';
+
+interface User {
+    name: string;
+    email: string;
+    avatar?: string;
+}
+
+const userSchema = new Schema<User>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    avatar: String,
+});
+
+export default userSchema;
+export{User, model}
+
