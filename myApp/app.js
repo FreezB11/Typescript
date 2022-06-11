2f41715c75e2fda3b21ae8b4abe4a632f4b05196
FreezB11 <yashrajrao4@gmail.com>
2022-06-11 23:17:20 +0530

    init

--- myApp/app.js
+++ myApp/app.js
@@ -19,6 +19,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require("express-session")({
+secret: "node js mongodb",
+resave: false,
+saveUninitialized: false
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
