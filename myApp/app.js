8943fc6715a98fd13df681d49e6a19308e347b22
FreezB11 <yashrajrao4@gmail.com>
2022-06-11 23:18:40 +0530

    init

--- myApp/app.js
+++ myApp/app.js
@@ -38,8 +38,69 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+app.get("/", function (req, res) {
+  res.render('register', {
+  title: 'Registration Page',
+  name: '',
+  email: '',
+  password: ''    
+  })
+  });
+  // Showing secret page
+  app.get("/home", isLoggedIn, function (req, res) {
+  res.render("home");
+  });
+  // Showing register form
+  app.get("/register", function (req, res) {
+  res.render('register', {
+  title: 'Registration Page',
+  name: '',
+  email: '',
+  password: ''    
+  })
+  });
+  // Handling user signup
+  app.post("/register", function (req, res) {
+  var email = req.body.email
+  var password = req.body.password
+  User.register(new User({ email: email }),
+  password, function (err, user) {
+  if (err) {
+  console.log(err);
+  return res.render("register");
+  }
+  passport.authenticate("local")(
+  req, res, function () {
+  req.flash('success', 'You have logged in')
+  res.render("home");
+  });
+  });
+  });
+  //Showing login form
+  app.get("/login", function (req, res) {
+  res.render('login', {
+  title: 'Login',
+  email: '',
+  password: ''     
+  })
+  });
+  //Handling user login
+  app.post("/login", passport.authenticate("local", {
+  successRedirect: "/home",
+  failureRedirect: "/login"
+  }), function (req, res) {
+  });
+  //Handling user logout
+  app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+  });
+  function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
+  }
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
