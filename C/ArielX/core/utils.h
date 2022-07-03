8540290ba3070b738e12ade06bf26d155fb9b4fc
FreezB11 <yashrajrao4@gmail.com>
2022-07-03 14:30:31 +0530

    init

---
+++ C/ArielX/core/utils.h
@@ -0,0 +1,55 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#if defined(PLATFORM_ANDROID)
+    #include <stdio.h>                      // Required for: FILE
+    #include <android/asset_manager.h>      // Required for: AAssetManager
+#endif
+
+#if defined(SUPPORT_TRACELOG)
+    #define TRACELOG(level, ...) TraceLog(level, __VA_ARGS__)
+
+    #if defined(SUPPORT_TRACELOG_DEBUG)
+        #define TRACELOGD(...) TraceLog(LOG_DEBUG, __VA_ARGS__)
+    #else
+        #define TRACELOGD(...) (void)0
+    #endif
+#else
+    #define TRACELOG(level, ...) (void)0
+    #define TRACELOGD(...) (void)0
+#endif
+
+//----------------------------------------------------------------------------------
+// Some basic Defines
+//----------------------------------------------------------------------------------
+#if defined(PLATFORM_ANDROID)
+    #define fopen(name, mode) android_fopen(name, mode)
+#endif
+
+//----------------------------------------------------------------------------------
+// Types and Structures Definition
+//----------------------------------------------------------------------------------
+//...
+
+//----------------------------------------------------------------------------------
+// Global Variables Definition
+//----------------------------------------------------------------------------------
+// Nop...
+
+//----------------------------------------------------------------------------------
+// Module Functions Declaration
+//----------------------------------------------------------------------------------
+#ifdef __cplusplus
+extern "C" {            // Prevents name mangling of functions
+#endif
+
+#if defined(PLATFORM_ANDROID)
+void InitAssetManager(AAssetManager *manager, const char *dataPath);   // Initialize asset manager from android app
+FILE *android_fopen(const char *fileName, const char *mode);           // Replacement for fopen() -> Read-only!
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // UTILS_H
