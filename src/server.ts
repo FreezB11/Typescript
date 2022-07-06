0ab26e800e2b54e1426b78f8c2f78f07abc873ac
FreezB11 <yashrajrao4@gmail.com>
2022-07-06 21:10:20 +0530

    init

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
 import express from 'express';
 import * as mongoose from 'mongoose';
+const passport = require('passport');
 
+
+const User = require('./models/user');
+passport.use(User.createStrategy());
+// Alternatively: passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
