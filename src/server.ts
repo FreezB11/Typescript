83fd8f05c47bdf5714041d2d4649bbcbcf091f7f
FreezB11 <yashrajrao4@gmail.com>
2022-07-06 22:06:20 +0530

    init

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 import * as mongoose from 'mongoose';
 const passport = require('passport');
+const LocalStrategy = require("passport-local")
+import bodyParser from 'body-parser';
+import User from '../models/User';
+import * as passportLocalMongoose from 'passport-local-mongoose';
+import http from 'http';
 
 
-const User = require('./models/user');
+mongoose.connect('mongodb+srv://yashraj:yashraj0403@cluster0.6vlbp.mongodb.net/?retryWrites=true&w=majority');
+
+
+const app = express();
+
+
 passport.use(User.createStrategy());
 // Alternatively: passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
