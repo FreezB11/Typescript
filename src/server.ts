5a5cbb0aba85239aedb5dc9a21567f8d970093bc
FreezB11 <yashrajrao4@gmail.com>
2022-07-08 20:27:27 +0530

    init

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from "express";
-import mongoose from "mongoose";
-import http from "http";
-
-mongoose.connect('mongodb+srv://yashraj:yashraj0403@cluster0.6vlbp.mongodb.net/?retryWrites=true&w=majority')
-
-console.log("started")
