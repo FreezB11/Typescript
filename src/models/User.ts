92a6d9a66fdd6bb9cb0c6a6bb71e95dd79f348cf
FreezB11 <yashrajrao4@gmail.com>
2022-07-08 19:07:51 +0530

    init

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,6 @@
 import { Document, model, Schema, PassportLocalModel, PassportLocalDocument, PassportLocalSchema} from "mongoose";
 
-import { IUserType } from "./UserType";
+// import { IUserType } from "./UserType";
 
 import passportLocalMongoose from "passport-local-mongoose";
 
@@ -9,10 +9,12 @@ export interface UserData {
       is_verified: boolean;
       status: boolean;
       username: string;
+      email: string;
   }
 
 const UserSchema = new Schema({
       username: { type: String, required: true },
+      email:{type: String, required: true },
       password: String,
       status: { type: Boolean, required: true },
       is_verified: { type: Boolean, required: true },
