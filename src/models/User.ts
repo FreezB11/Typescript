d68e9ae1112d078536681fa88192cdf8e6d0b189
FreezB11 <yashrajrao4@gmail.com>
2022-07-08 19:54:47 +0530

    init

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,16 +9,14 @@ export interface UserData {
       is_verified: boolean;
       status: boolean;
       username: string;
-      email: string;
   }
 
 const UserSchema = new Schema({
       username: { type: String, required: true },
-      email:{type: String, required: true },
       password: String,
       status: { type: Boolean, required: true },
-      is_verified: { type: Boolean, required: false },
-      is_temporary: { type: Boolean, required: false }
+      is_verified: { type: Boolean, required: true },
+      is_temporary: { type: Boolean, required: true }
     });
 
 interface UserModel <T extends Document> extends PassportLocalModel<T> {}
