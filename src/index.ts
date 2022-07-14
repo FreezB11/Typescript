bf88ed883f31a442c1c7f185f793c54804ce8c75
FreezB11 <83819164+FreezB11@users.noreply.github.com>
2022-07-14 16:05:07 +0000

    init

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './pre-start'; // Must be the first import
 import logger from 'jet-logger';
 import server from './server';
 
-
+//test
 // Constants
 const serverStartMsg = 'Express server started on port: ',
         port = (process.env.PORT || 3000);
