3d22c2db5b273fa6483bbe4550ffa16d9968c63d
FreezB11 <83819164+FreezB11@users.noreply.github.com>
2022-07-15 12:50:02 +0000

    init

--- src/shared/functions.ts
+++ src/shared/functions.ts
@@ -1,11 +1,5 @@
 import logger from 'jet-logger';
 
-
-/**
- * Print an error object if it's truthy. Useful for testing.
- * 
- * @param err 
- */
 export function pErr(err?: Error): void {
     if (!!err) {
         logger.err(err);
