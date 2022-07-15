3cdb2c4d6203c87d0b329a30415daa4085383f61
FreezB11 <83819164+FreezB11@users.noreply.github.com>
2022-07-15 12:47:02 +0000

    init

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,6 +1,5 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response, Router } from 'express';
-
 import userService from '@services/user-service';
 import { ParamMissingError } from '@shared/errors';
 
