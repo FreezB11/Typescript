75cb5d5e95b0322d0d6f88128be8d67b21bb6cd8
FreezB11 <83819164+FreezB11@users.noreply.github.com>
2022-07-15 12:44:05 +0000

    init

--- src/pre-start/index.ts
+++ src/pre-start/index.ts
@@ -1,8 +1,3 @@
-/**
- * Pre-start is where we want to place things that must run BEFORE the express server is started.
- * This is useful for environment variables, command-line arguments, and cron-jobs.
- */
-
 import path from 'path';
 import dotenv from 'dotenv';
 import commandLineArgs from 'command-line-args';
