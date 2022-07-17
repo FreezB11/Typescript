759605e5c6b05d2e7d1a5102fa28b23d2d95aa42
FreezB11 <yashrajrao4@gmail.com>
2022-07-17 09:35:27 +0000

    init

--- c/main.c
+++ c/main.c
@@ -1,42 +1,33 @@
-#include<stdio.h>
-#include<string.h>	//strlen
+#include<stdio.h> //printf
+#include<string.h> //strcpy
 #include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
+#include<netdb.h>	//hostent
+#include<arpa/inet.h>
 
 int main(int argc , char *argv[])
 {
-	int socket_desc;
-	struct sockaddr_in server;
-	char *message;
-	
-	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc == -1)
-	{
-		printf("Could not create socket");
-	}
+	char *hostname = "www.google.com";
+	char ip[100];
+	struct hostent *he;
+	struct in_addr **addr_list;
+	int i;
 		
-	server.sin_addr.s_addr = inet_addr("74.125.235.20");
-	server.sin_family = AF_INET;
-	server.sin_port = htons( 80 );
-
-	//Connect to remote server
-	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+	if ( (he = gethostbyname( hostname ) ) == NULL) 
 	{
-		puts("connect error");
+		//gethostbyname failed
+		herror("gethostbyname");
 		return 1;
 	}
 	
-	puts("Connected\n");
+	//Cast the h_addr_list to in_addr , since h_addr_list also has the ip address in long format only
+	addr_list = (struct in_addr **) he->h_addr_list;
 	
-	//Send some data
-	message = "GET / HTTP/1.1\r\n\r\n";
-	if( send(socket_desc , message , strlen(message) , 0) < 0)
+	for(i = 0; addr_list[i] != NULL; i++) 
 	{
-		puts("Send failed");
-		return 1;
+		//Return the first one;
+		strcpy(ip , inet_ntoa(*addr_list[i]) );
 	}
-	puts("Data Send\n");
 	
+	printf("%s resolved to : %s" , hostname , ip);
 	return 0;
 }
