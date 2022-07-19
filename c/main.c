e654c3e8b047e2f469f53a77009a0af0586926aa
FreezB11 <yashrajrao4@gmail.com>
2022-07-19 15:04:32 +0000

    init

--- c/main.c
+++ c/main.c
@@ -23,7 +23,12 @@ int main(){
     getaddrinfo(0, "8080", &hints, &bind_address);
 
     int socket_listen;
-
+    socket_listen = socket(bind_address->ai_family,
+            bind_address->ai_socktype, bind_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_listen)) {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
     return 0;
 
 }
