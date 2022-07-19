c9a8cf4d92e2440e7c3dba7a482defbdd066d77f
FreezB11 <yashrajrao4@gmail.com>
2022-07-19 15:03:59 +0000

    init

--- c/http.h
+++ c/http.h
@@ -1,6 +1,36 @@
-#ifndef HTTP
-#define HTTP
-// Required Libraries
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Lewis Van Winkle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#else
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,228 +38,23 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <errno.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
 
-// defines
-#define MAX_REQUEST_SIZE 2047
-#define SOCKET int
+#endif
+
+
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
 #define ISVALIDSOCKET(s) ((s) >= 0)
 #define CLOSESOCKET(s) close(s)
 #define SOCKET int
 #define GETSOCKETERRNO() (errno)
-
-
-
-
-// structs
-struct client_info {
-    socklen_t address_length;
-    struct sockaddr_storage address;
-    SOCKET socket;
-    char request[MAX_REQUEST_SIZE + 1];
-    int received;
-    struct client_info *next;
-};
-
-static struct client_info *clients = 0;
-
-struct client_info *get_client(SOCKET s) {
-    struct client_info *ci = clients;
-
-    while(ci) {
-        if (ci->socket == s)
-            break;
-        ci = ci->next;
-    }
-
-    if (ci) return ci;
-    struct client_info *n =
-        (struct client_info*) calloc(1, sizeof(struct client_info));
-
-    if (!n) {
-        fprintf(stderr, "Out of memory.\n");
-        exit(1);
-    }
-
-    n->address_length = sizeof(n->address);
-    n->next = clients;
-    clients = n;
-    return n;
-}
-const char *get_client_address(struct client_info *ci) {
-    static char address_buffer[100];
-    getnameinfo((struct sockaddr*)&ci->address,
-            ci->address_length,
-            address_buffer, sizeof(address_buffer), 0, 0,
-            NI_NUMERICHOST);
-    return address_buffer;
-}
-
-void drop_client(struct client_info *client) {
-    CLOSESOCKET(client->socket);
-
-    struct client_info **p = &clients;
-
-    while(*p) {
-        if (*p == client) {
-            *p = client->next;
-            free(client);
-            return;
-        }
-        p = &(*p)->next;
-    }
-
-    fprintf(stderr, "drop_client not found.\n");
-    exit(1);
-}
-// file type
-const char *file_type(const char* path) {
-    const char *last_dot = strrchr(path, '.');
-    if (last_dot) {
-        if (strcmp(last_dot, ".css") == 0) return "text/css";
-        if (strcmp(last_dot, ".csv") == 0) return "text/csv";
-        if (strcmp(last_dot, ".gif") == 0) return "image/gif";
-        if (strcmp(last_dot, ".htm") == 0) return "text/html";
-        if (strcmp(last_dot, ".html") == 0) return "text/html";
-        if (strcmp(last_dot, ".ico") == 0) return "image/x-icon";
-        if (strcmp(last_dot, ".jpeg") == 0) return "image/jpeg";
-        if (strcmp(last_dot, ".jpg") == 0) return "image/jpeg";
-        if (strcmp(last_dot, ".js") == 0) return "application/javascript";
-        if (strcmp(last_dot, ".json") == 0) return "application/json";
-        if (strcmp(last_dot, ".png") == 0) return "image/png";
-        if (strcmp(last_dot, ".pdf") == 0) return "application/pdf";
-        if (strcmp(last_dot, ".svg") == 0) return "image/svg+xml";
-        if (strcmp(last_dot, ".txt") == 0) return "text/plain";
-    }
-
-    return "application/octet-stream";
-}
-
-SOCKET create_socket(const char* host, const char *port) {
-    printf("Configuring local address...\n");
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    struct addrinfo *bind_address;
-    getaddrinfo(host, port, &hints, &bind_address);
-
-    printf("Creating socket...\n");
-    SOCKET socket_listen;
-    socket_listen = socket(bind_address->ai_family,
-            bind_address->ai_socktype, bind_address->ai_protocol);
-    if (!ISVALIDSOCKET(socket_listen)) {
-        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
-        exit(1);
-    }
-
-    printf("Binding socket to local address...\n");
-    if (bind(socket_listen,
-                bind_address->ai_addr, bind_address->ai_addrlen)) {
-        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
-        exit(1);
-    }
-    freeaddrinfo(bind_address);
-
-    printf("Listening...\n");
-    if (listen(socket_listen, 10) < 0) {
-        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
-        exit(1);
-    }
-
-    return socket_listen;
-}
-
-void send_400(struct client_info *client) {
-    const char *c400 = "HTTP/1.1 400 Bad Request\r\n"
-        "Connection: close\r\n"
-        "Content-Length: 11\r\n\r\nBad Request";
-    send(client->socket, c400, strlen(c400), 0);
-    drop_client(client);
-}
-
-void send_404(struct client_info *client) {
-    const char *c404 = "HTTP/1.1 404 Not Found\r\n"
-        "Connection: close\r\n"
-        "Content-Length: 9\r\n\r\nNot Found";
-    send(client->socket, c404, strlen(c404), 0);
-    drop_client(client);
-}
-
-void render(struct client_info *client, const char *path) {
-
-    printf("serve_resource %s %s\n", get_client_address(client), path);
-
-    if (strcmp(path, "/") == 0) path = "/index.html";
-
-    if (strlen(path) > 100) {
-        send_400(client);
-        return;
-    }
-
-    if (strstr(path, "..")) {
-        send_404(client);
-        return;
-    }
-
-    char full_path[128];
-    sprintf(full_path, "public%s", path);
-
-#if defined(_WIN32)
-    char *p = full_path;
-    while (*p) {
-        if (*p == '/') *p = '\\';
-        ++p;
-    }
 #endif
 
-    FILE *fp = fopen(full_path, "rb");
 
-    if (!fp) {
-        send_404(client);
-        return;
-    }
-
-    fseek(fp, 0L, SEEK_END);
-    size_t cl = ftell(fp);
-    rewind(fp);
-
-    const char *ct = file_type(full_path);
-
-#define BSIZE 1024
-    char buffer[BSIZE];
-
-    sprintf(buffer, "HTTP/1.1 200 OK\r\n");
-    send(client->socket, buffer, strlen(buffer), 0);
-
-    sprintf(buffer, "Connection: close\r\n");
-    send(client->socket, buffer, strlen(buffer), 0);
-
-    sprintf(buffer, "Content-Length: %u\r\n", cl);
-    send(client->socket, buffer, strlen(buffer), 0);
-
-    sprintf(buffer, "Content-Type: %s\r\n", ct);
-    send(client->socket, buffer, strlen(buffer), 0);
-
-    sprintf(buffer, "\r\n");
-    send(client->socket, buffer, strlen(buffer), 0);
-
-    int r = fread(buffer, 1, BSIZE, fp);
-    while (r) {
-        send(client->socket, buffer, r, 0);
-        r = fread(buffer, 1, BSIZE, fp);
-    }
-
-    fclose(fp);
-    drop_client(client);
-}
-
-
-
-
-#endif
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
--- c/main.c
+++ c/main.c
@@ -9,3 +9,21 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+
+int main(){
+
+    printf("Configuring local address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    struct addrinfo *bind_address;
+    getaddrinfo(0, "8080", &hints, &bind_address);
+
+    int socket_listen;
+
+    return 0;
+
+}
