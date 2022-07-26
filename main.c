05509d3f7ed36f5efcd2ffe5173af860b04d5685
FreezB11 <yashrajrao4@gmail.com>
2022-07-26 19:14:15 +0530

    init

--- main.c
+++ main.c
@@ -2,26 +2,8 @@
 
 OXE main()
 {
-    // Display* display = XOpenDisplay(NILL);
-    // Window window = XCreateSimpleWindow(display, 
-    // XDefaultRootWindow(display),
-    // 100, 100, 200, 200, 4, 0, 0);
-    
-
-    // // window title
-    // XStoreName(display, window, "title");
-
+  
     Create_window(100,100,"title");
-
-    // XEvent event;
-
-    // XMapWindow(display, window);
-    // // XSelectInput(display, window, KeyPressMask | ButtonPressMask | ExposureMask);
-
-    // while (TRUE) {
-    //   XNextEvent(display, &event);
-    //   printf("%d\n", event.type);
-    // }
 
     return 0;
 }
